9908456fd223d360a07605b2d34d3fcfc3b0a384
JustHobbs <rafaelmassarollo@gmail.com>
2022-09-22 12:12:59 -0300

    Att

--- Scripts/klmoverComSinc.bat
+++
@@ -1,3 +0,0 @@
-start cmd /c  ""C:\Program Files (x86)\Kaspersky Lab\NetworkAgent\klmover.exe"" -address InserirIP
-timeout 5
-start cmd /c ""C:\Program Files (x86)\Kaspersky Lab\NetworkAgent\klnagchk.exe"" -sendhb
