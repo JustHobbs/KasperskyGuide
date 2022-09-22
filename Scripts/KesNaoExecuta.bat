cec7081488b5ff721f440367723534cfc1be0f8d
JustHobbs <rafaelmassarollo@gmail.com>
2022-09-22 12:04:41 -0300

    v2

--- Scripts/KesNaoExecuta.bat
+++ Scripts/KesNaoExecuta.bat
@@ -1,7 +1,13 @@
+## README ##
+# Script criado com a função de solucionar os status: A proteção está desativada ou O aplicativo de segurança não está em execução
+# O problema as vezes é ocasionado pois alguma falha aconteceu durante a inicialização e o endpoint não abriu
+# Para solucionar isso o script força o inicio do endpoint no dispositivo
+
+
 #x86
 start cmd /c ""C:\Program Files (x86)\Kaspersky Lab\NetworkAgent\klpsm.exe"" start_avp_service
 timeout 5
-start cmd /c "C:\Program Files (x86)\Kaspersky Lab\Kaspersky Endpoint Security for Windows\avpui.exe”
+start cmd /c ""C:\Program Files (x86)\Kaspersky Lab\Kaspersky Endpoint Security for Windows\avpui.exe”"
 timeout 5
 start cmd /c ""C:\Program Files (x86)\Kaspersky Lab\NetworkAgent\klmover.exe"" -address InserirIP
 timeout 5
