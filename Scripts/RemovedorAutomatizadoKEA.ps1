eaeec38fdd96ddf9fccdf989586ae712a3893488
JustHobbs <rafaelmassarollo@gmail.com>
2022-09-22 12:19:41 -0300

    add

--- Scripts/RemovedorAutomatizadoKEA.ps1
+++ Scripts/RemovedorAutomatizadoKEA.ps1
@@ -1,3 +1,11 @@
+################################# README ################################
+# Script criado com o intuito de remover o KEA quando a tarefa de remoção
+# não funciona.
+# Utiliza o msiexec para realizar a remoção através de GUID
+#
+#########################################################################
+
+
 #Identificar a Versao e o GUID do Kaspersky Endpoint Agent
 Write-host "Identificando o caminho do Kaspersky Endpoint Agent..."
 $AppInfo = Get-WmiObject Win32_Product -Filter "Name Like '%Kaspersky Endpoint Agent%'"
